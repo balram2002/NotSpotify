c239dff37b16a4feea2bd7099a7d2bf56c2c8da7
Sufiyan Ansari <sufiyanansari1949@gmail.com>
2022-11-08 21:26:40 +0530

    random background color feature added to search screen category card

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ playBtns.forEach((btn) => {
 
 navBtns.forEach((btn) => {
     btn.addEventListener('click', function () {
-        
+
         var current = document.getElementsByClassName("sideBarActive");
         current[0].className = current[0].className.replace(" sideBarActive", "");
         this.className += " sideBarActive";
@@ -20,6 +20,24 @@ navBtns.forEach((btn) => {
         route(this.id);
     });
 });
+
+
+
+
+function random_bg_color() {
+    // Get a random number between 64 to 256
+    // (for getting lighter colors)
+
+    let red = Math.floor(Math.random() * 256) + 54;
+    let green = Math.floor(Math.random() * 256) + 54;
+    let blue = Math.floor(Math.random() * 256) + 54;
+
+    // Construct a color with the given values
+    let bgColor = "rgb(" + red + ", " + green + ", " + blue + ")";
+
+    // Set the background to the new color
+    return bgColor;
+}
 
 
 function togglePPIcon(id) {
@@ -40,6 +58,14 @@ function route(id) {
             document.getElementById("content").innerHTML = this.responseText;
         }
     };
+    xhttp.onload = function () {
+        if (id == 'search') {
+            const catCards = document.querySelectorAll('.catCard');
+            catCards.forEach((card) => {
+                card.style.backgroundColor = random_bg_color();
+            });
+        }
+    }
     xhttp.open("GET", "pages/" + id + ".txt", true);
     xhttp.send();
 }
