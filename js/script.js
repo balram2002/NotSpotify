c00ac904150c2ef522971a34c65f5f25079d755e
Sufiyan1949 <56268355+Sufiyan1949@users.noreply.github.com>
2022-11-18 06:02:00 +0530

    Login page added and folder structure changed..

--- js/script.js
+++ js/script.js
@@ -1,22 +1,14 @@
-
-const playBtns = document.querySelectorAll('.playPauseBtn');
-const navBtns = document.querySelectorAll('.navItem, .navItem2');
-
-
-playBtns.forEach((btn) => {
+document.querySelectorAll('.playPauseBtn').forEach((btn) => {
     btn.addEventListener('click', function () {
-        id = this.querySelectorAll("Img")[0].id;
-        togglePPIcon(id);
+        togglePPIcon(this.querySelector("Img").id);
     });
 });
 
-navBtns.forEach((btn) => {
+document.querySelectorAll('.navItem, .navItem2').forEach((btn) => {
     btn.addEventListener('click', function () {
-
         var current = document.getElementsByClassName("sideBarActive");
         current[0].className = current[0].className.replace(" sideBarActive", "");
         this.className += " sideBarActive";
-
         route(this.id);
     });
 });
@@ -25,29 +17,20 @@ navBtns.forEach((btn) => {
 
 
 function random_bg_color() {
-    // Get a random number between 64 to 256
-    // (for getting lighter colors)
-
     let red = Math.floor(Math.random() * 176) + 10
     let green = Math.floor(Math.random() * 176) + 10;
     let blue = Math.floor(Math.random() * 176) + 10;
-
-    // Construct a color with the given values
     let bgColor = "rgb(" + red + ", " + green + ", " + blue + ")";
-
-    // Set the background to the new color
     return bgColor;
 }
 
-
 function togglePPIcon(id) {
     var img = document.getElementById(id).src;
-    if (img.indexOf('assets/ic_play.svg') != -1) {
+    if (img.indexOf('assets/ic_play.svg') != -1)
         document.getElementById(id).src = 'assets/ic_pause.svg';
-    }
-    else {
+    else
         document.getElementById(id).src = 'assets/ic_play.svg';
-    }
+
 }
 
 function route(id) {
@@ -69,3 +52,10 @@ function route(id) {
     xhttp.open("GET", "pages/" + id + ".txt", true);
     xhttp.send();
 }
+
+/* let audio = new Audio("http://21273.live.streamtheworld.com/LOS40_DANCE.mp3");
+
+let volume = document.querySelector("#volume");
+volume.addEventListener("change", function(e) {
+audio.volume = e.currentTarget.value / 100;
+}) */
