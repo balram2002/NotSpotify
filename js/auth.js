ebca655c53f7f9fcabcd4f190ac36c32ed1c756a
Ahmed1084 <109950387+Ahmed1084@users.noreply.github.com>
2022-11-20 01:44:17 +0530

    singUp code added

---
+++ js/auth.js
@@ -0,0 +1,141 @@
+import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js'
+import {firebaseConfig} from "./firebase-config.js";
+import {
+    getAuth, signInWithRedirect, getRedirectResult, signInWithPopup, createUserWithEmailAndPassword,
+    GoogleAuthProvider, FacebookAuthProvider, signInWithEmailAndPassword, sendEmailVerification, updateProfile
+}
+    from 'https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js';
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+var user;
+console.log("Auth.js");
+
+function getData(elements) {
+    const obj = {};
+    elements.forEach((e) => obj[e.name] = e.value);
+    return obj;
+}
+
+const setError = (element) => element.style.borderColor = "red";
+
+
+function validation(elements) {
+    let c = null;
+    elements.forEach((e) => {
+        switch (e.name) {
+            case "email" :
+                if (e.value === "") setError(c = e);
+                break;
+            case "cEmail" :
+                if (e.value === "" || e.value !== elements.at(0).value) setError(c = e);
+                break;
+            case "userName" :
+                if (e.value === "") setError(c = e);
+                break;
+            case "password" :
+                if (e.value.length < 8) setError(c = e);
+                break;
+            case "DOB" :
+                if (e.value === "") setError(c = e);
+                break;
+        }
+    });
+    if (c == null) return getData(elements);
+    else return null;
+
+}
+
+/* Login & Signup Using Firebase Email Verification */
+
+document.querySelector(".btnDiv>.btn").onclick =
+    function emailPassword_singIn() {
+        console.log("singIn");
+        const elements = document.querySelectorAll(".inputCtnr");
+        const ele = [];
+        elements.forEach((element) => ele.push(element.getElementsByTagName("input")[0]));
+        let inputFieldsData = validation(ele);
+        let gen = null;
+        document.querySelectorAll('input[name="gender"]').forEach(g => {
+            if (g.checked) gen = g.value;
+        });
+        if (gen == null) {
+            setError(document.querySelector(".genderCtnr"));
+            return;
+        } else inputFieldsData["gender"] = gen;
+        /*createUserWithEmailAndPassword(auth, inputFieldsData.email, inputFieldsData.password)
+            .then((userCredential) => {
+                user = userCredential.user;
+                updateProfile(user, {
+                    displayName: inputFieldsData.name, photoURL: ""
+                }).then(() => {
+
+                }).catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    console.log(errorCode + "errorCode");
+                    console.log(errorMessage + "errorMessage");
+                });
+                sendEmailVerification(user)
+                    .then(() => {
+                        console.log("Email Sent");
+                    });
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode + "errorCode");
+                console.log(errorMessage + "errorMessage");
+            });*/
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", "http://musify.42web.io/Api%27s/singUpUser.php", true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.send(
+            "name = "+inputFieldsData.name+
+            "&email="+ inputFieldsData.email+
+            "&password"+ inputFieldsData.password+
+            "&gender"+ inputFieldsData.gender+
+            "&DOB"+ inputFieldsData.gender
+        );
+
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                console.log(this.responseText);
+            }
+        };
+        xhttp.onload = function () {
+
+
+        }
+
+    }
+
+
+/*
+
+
+
+
+
+/*
+//Email & Password Authentication
+document.getElementById("btn3").onclick = function atts() {
+var email = document.getElementById("e").value;
+var pass = document.getElementById("p").value;
+console.log("SignUp" + email.toString() + pass.toString());
+signInWithEmailAndPassword(auth, email, pass)
+.then((userCredential) => {
+ // Signed in
+ const user = userCredential.user;
+ console.log(user);
+ console.log(user.uid);
+ // ...
+})
+.catch((error) => {
+ const errorCode = error.code;
+ const errorMessage = error.message;
+ console.log(errorCode + "errorCode");
+ console.log(errorMessage + "errorMessage");
+});
+}
+*/
---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDObnvYDI9bwWyRta3vDBLSTxOhtdgQ5dQ",
+    authDomain: "notspotify-d281f.firebaseapp.com",
+    projectId: "notspotify-d281f",
+    storageBucket: "notspotify-d281f.appspot.com",
+    messagingSenderId: "331732154949",
+    appId: "1:331732154949:web:51a069621d3b814d2ec12c",
+    measurementId: "G-NKRWQLD1EX"
+};
